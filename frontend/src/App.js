3af881ecb80cae4b5aa6b1c3fb31832e969987e8
marwane slimi <marwaneslimii@gmail.com>
2023-06-27 18:21:21 +0100

    Part3

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Bi3a w charya</a>
+      </header>
+      <main>Products list</main>
+    </div>
+  );
+}
+
+export default App;
