c972c0b12eb5b5ff9fb9f999047a38298ea41f5e
marwane slimi <marwaneslimii@gmail.com>
2023-07-01 00:10:51 +0100

    Product Screen development

---
+++ backend/data.js
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      name: 'iPhone14',
+      slug: 'iPhone-14',
+      category: 'smartPhone',
+      image: '/images/iPhone14.jpg',
+      price: 1000,
+      countInStock: 5,
+      brand: 'iPhone',
+      rating: 4,
+      numReviews: 100,
+      description: 'iPhone 14',
+    },
+    {
+      name: 'sumsung a50',
+      slug: 'sumsung-a-50',
+      category: 'smartPhone',
+      image: '/images/sumsungA50.jpg',
+      price: 900,
+      countInStock: 5,
+      brand: 'sumsung',
+      rating: 3,
+      numReviews: 150,
+      description: 'sumsung a50',
+    },
+  ],
+};
+
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,35 @@
-import data from './data';
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Bi3a w charya</a>
-      </header>
-      <main>
-        <h1>Products list</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Bi3a w charya</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved © 2023</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
