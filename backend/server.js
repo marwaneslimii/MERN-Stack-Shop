c11029d2c4b8acd1bf84c051109860b7e72356de
marwane slimi <marwaneslimii@gmail.com>
2023-07-20 00:07:50 +0100

    Product management

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
-import uploadRouter from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -26,7 +25,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/upload', uploadRouter);
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
